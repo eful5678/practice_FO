fd97995e07b05fb1ff59be2e01ef1ee7fe77a1db
Jin <khjkhj1212@hanmail.net>
2021-05-17 21:18:32 +0900

    hi

---
+++ VUE-ENDGAME/vue-til/src/components/SignupForm.vue
@@ -0,0 +1,46 @@
+.<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id : </label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw : </label>
+      <input type="text" id="password" v-model="password" />
+    </div>
+    <div>
+      <label for="nickname">nickname : </label>
+      <input type="text" id="nickname" v-model="nickname" />
+    </div>
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import { registerUser } from "@/api/index";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      nickname: "",
+    };
+  },
+
+  methods: {
+    async submitForm() {
+      console.log("submit form");
+      const userData = {
+        username: this.username,
+        password: this.password,
+        nickname: this.nickname,
+      };
+      const response = await registerUser(userData);
+      console.log(response);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ VUE-ENDGAME/vue-til/src/components/common/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/signup">Sign up</router-link>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
