ecdf7f823020221d27c01af02c610e97a65f6c96
Jin <khjkhj1212@hanmail.net>
2021-05-20 23:15:28 +0900

    server

---
+++ VUE-ENDGAME/vue-til-server/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  extends: ['prettier', 'plugin:prettier/recommended'],
+  rules: {
+
+
+  },
+};
